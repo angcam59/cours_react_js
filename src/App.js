a2d1ec05c2a500bc440a68e828d3bbb1bf03535c
angcam59 <guerra.angelo@orange.fr>
2023-01-09 16:55:16 +0100

    1er commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Page1 from'./pages/Page1';
+
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/page1" exact element={<Page1 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
   );
 }
 
